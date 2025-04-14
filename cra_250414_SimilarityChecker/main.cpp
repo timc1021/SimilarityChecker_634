b836b27fc74e8c1e2bc560c3287e2c950f6620e6
timc1021 <iljfyy@gmail.com>
2025-04-14 09:23:43 +0900

    environment setup

--- cra_250414_SimilarityChecker/main.cpp
+++ cra_250414_SimilarityChecker/main.cpp
@@ -1,5 +1,12 @@
 #include "iostream"
+#include "gmock/gmock.h"
+#include "similarityChecker.cpp"
+
+TEST(simChecker, TC1) {
+	EXPECT_EQ(1, 1);
+}
 
 int main(void) {
-	return 0;
+	::testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
 }
